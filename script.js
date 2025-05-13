c5afe880fae5cf67766a0c0b4b0f8a2105a17843
Anjesh <anjeshpathak2021@gmail.com>
2025-05-13 12:29:06 +0530

    added files

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const nav = document.getElementById('nav-links');
+    nav.classList.toggle('active');
+  }
+//   added
+function toggleDropdown() {
+    const dropdown = document.getElementById('dropdown');
+    dropdown.classList.toggle('active');
+  }  
